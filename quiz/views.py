387ddc0ac82fb256cdbe39b6fecfff1a02804b7b
kanilss <karinnilsso@gmail.com>
2016-09-08 11:37:45 +0200

    quiz-sidan använder data

--- quiz/views.py
+++ quiz/views.py
@@ -13,8 +13,8 @@ quizzes = [
 	},
 	{
 		"quiz_number": 3,
-   	    	"name": "Världens mest kända hackare",
-	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+   	    "name": "Världens mest kända hackare",
+	    "description": "Hackerhistoria är viktigt, kan du den?"	},
 ]
 
 # Create your views here.
@@ -24,8 +24,12 @@ def startpage(request):
 	}
 	return render(request, "quiz/start.html", context)
 
-def quiz(request):
-	return render(request, "quiz/quiz.html")
+def quiz(request, quiz_number):
+	context = {
+		"quiz": quizzes[int(quiz_number) - 1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/quiz.html", context)
 
 def question(request):
 	return render(request, "quiz/question.html")
