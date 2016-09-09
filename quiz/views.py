de6549f1b3ef4513589493e0a8f0cf25ece101d2
kanilss <karinnilsso@gmail.com>
2016-09-09 10:45:23 +0200

    Andrat views-questions

--- quiz/views.py
+++ quiz/views.py
@@ -27,18 +27,22 @@ def startpage(request):
 
 def quiz(request, quiz_number):
 	context = {
-		"quiz": quizzes[int(quiz_number) - 1],
+		"quiz": Quiz.objects.get(quiz_number = quiz_number),
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/quiz.html", context)
 
 def question(request, quiz_number, question_number):
+	quiz = Quiz.objects.get(quiz_number = quiz_number)
+	questions = quiz.questions.all()
+	question = questions[int(question_number) - 1]
 	context = {
 		"question_number": question_number,
-		"question": "Hur många bultar har ölandsbron?",
-		"answer1": "12",
-		"answer2": "66 400",
-		"answer3": "7 428 954",
+		"question": question,
+		"answer1": question.answer1,
+		"answer2": question.answer2,
+		"answer3": question.answer3,
+		"quiz": quiz,
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/question.html", context)
